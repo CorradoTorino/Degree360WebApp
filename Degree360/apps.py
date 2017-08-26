70520a90d1fbab577592f069f45fd34627161cd0
Corrado <corrado.torino@gmail.com>
2017-08-26 18:24:42 +0200

    create model for FeedbackProvider and migrated it in the db

--- Degree360/apps.py
+++ Degree360/apps.py
@@ -1,5 +1,4 @@
 from django.apps import AppConfig
 
-
 class Degree360Config(AppConfig):
     name = 'Degree360'
--- Degree360/models.py
+++ Degree360/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+    
+class RelationType(models.Model):
+    relation_type = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    
+class FeedbackProvider(models.Model):
+    name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    relation_type = models.ForeignKey(RelationType, on_delete=models.CASCADE)
 
-# Create your models here.
