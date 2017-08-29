4cdc200e03529d5574278defee2e4994fe3f9978
Corrado C <corrado.torino@gmail.com>
2017-08-30 00:42:14 +0200

    Retrieving feedbackProviders name in RequestFeedback view

--- Degree360/views.py
+++ Degree360/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
+from Degree360.models import FeedbackProvider
 
 def workInProgress(request):
     return HttpResponse("Generic view for Degree360. Work in progress.")
 
 def requestFeedaback(request, pk):
-    return HttpResponse("!!requestFeedaback view.. work in progress!! You're looking at 360 Degree feedback survey %s." % pk)
+    feedbackProviders = FeedbackProvider.objects.all()
+    pseudoHead = "!!requestFeedaback view.. work in progress!! You're looking at 360 Degree feedback survey %s." % pk
+    names = ', '.join([fp.name for fp in feedbackProviders])
+    output = pseudoHead + names
+    return HttpResponse(output)
