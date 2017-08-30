9533d016e7b1a880099a7fbc71c39f98ddc5e75a
Corrado C <corrado.torino@gmail.com>
2017-08-30 23:41:02 +0200

    Updated model to add concept of survey

--- Degree360/admin.py
+++ Degree360/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from Degree360.models import RelationType, FeedbackProvider
+from Degree360.models import RelationType, FeedbackProvider, Survey
 
 # Register your models here.
+admin.site.register(Survey)
 admin.site.register(RelationType)
 admin.site.register(FeedbackProvider)
--- Degree360/models.py
+++ Degree360/models.py
@@ -1,8 +1,20 @@
 from django.db import models
+
+import uuid
+
+class Survey(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    employee_name = models.CharField(max_length=100, blank=True, default='')
+    employee_last_name = models.CharField(max_length=100, blank=True, default='')
+    employee_email = models.EmailField(max_length=100)
+    
+    def __str__(self):
+        return '{} {}'.format(self.employee_name, self.employee_last_name)
     
 class RelationType(models.Model):
     relation_type = models.CharField(max_length=50)
     description = models.CharField(max_length=200)
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
     
     def __str__(self):
         return '{}'.format(self.relation_type, self.description) 
@@ -12,6 +24,7 @@ class FeedbackProvider(models.Model):
     last_name = models.CharField(max_length=100, blank=True, default='')
     email = models.EmailField(max_length=100)
     relation_type = models.ForeignKey(RelationType, on_delete=models.CASCADE)
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
     
     def __str__(self):
         return '{} {} ({})'.format(self.name, self.last_name, self.email) 
