4aae8a3086d6c32252bc35420fe47366a8e36b35
Calzoni <calzonic@emea.roche.com>
2017-09-04 23:26:50 +0200

    Solved issue on unique_together of survey and emailfor FeedbackProvider

--- Degree360/models.py
+++ Degree360/models.py
@@ -1,20 +1,33 @@
 from django.db import models
 
 import uuid
-
+    
 class Survey(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     employee_name = models.CharField(max_length=100, blank=True, default='')
     employee_last_name = models.CharField(max_length=100, blank=True, default='')
     employee_email = models.EmailField(max_length=100)
     
+    @classmethod
+    def create(cls, name, lastName, email):
+        instance = cls(employee_name=name, employee_last_name=lastName, employee_email=email)
+        instance.save()
+        return instance
+        
     def __str__(self):
         return '{} {}'.format(self.employee_name, self.employee_last_name)
+    
     
 class RelationType(models.Model):
     relation_type = models.CharField(max_length=50)
     description = models.CharField(max_length=200)
     survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
+    
+    @classmethod
+    def create(cls, relation_type, description, survey):
+        instance = cls(relation_type=relation_type, description=description, survey=survey)
+        instance.save()
+        return instance
     
     def __str__(self):
         return '{}'.format(self.relation_type, self.description) 
@@ -25,10 +38,17 @@ class FeedbackProvider(models.Model):
     email = models.EmailField(max_length=100)
     relation_type = models.ForeignKey(RelationType, on_delete=models.CASCADE)
     survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
-    
-    unique_together = (("survey", "email"),)
+        
+    @classmethod
+    def create(cls, name, last_name, email, relation_type, survey):
+        instance = cls(name=name, last_name=last_name, email=email, relation_type=relation_type, survey=survey)
+        instance.save()
+        return instance
     
     def __str__(self):
-        return '{} {} ({})'.format(self.name, self.last_name, self.email) 
+        return '{} {} ({})'.format(self.name, self.last_name, self.email)
+    
+    class Meta:
+        unique_together = (("survey", "email"),)
 
 
--- Degree360/testModels.py
+++ Degree360/testModels.py
@@ -1,39 +1,48 @@
 import unittest
+from django.test import TestCase
+from django.db import IntegrityError
 
-from .models import RelationType
-from Degree360.models import FeedbackProvider
-
-def CreateDummyRelationPeer():
-    aRelation = RelationType()
-    aRelation.relation_type = 'DummyPeer'
-    aRelation.description = 'A dummy colleague'
-    return aRelation
+from Degree360.models import FeedbackProvider, Survey, RelationType
     
-def CreateDummmyFeedbackProvider(name = 'DummyName', email = 'dummy@myEmail.com'):
-    aFeedbackProvider = FeedbackProvider()
-    aFeedbackProvider.name = name    
-    aFeedbackProvider.relation_type = CreateDummyRelationPeer()
-    aFeedbackProvider.email = email
+def CreateDummmyFeedbackProvider(name = 'DummyName', email = 'dummy@myEmail.com'):  
+    aSurvey = Survey.create('dummyEmployName', 'dummyEmployLastName', 'dummyEmploy@myEmail.com')
+    aRelation = RelationType.create('DummyPeer', 'A dummy colleague', aSurvey)
+    aFeedbackProvider = FeedbackProvider.create(name, '', email, aRelation, aSurvey)
     return aFeedbackProvider
         
-class Test(unittest.TestCase):
-
-    def testName(self):
-        pass
+class Test(TestCase):
     
     def test_RelationType__str__(self):
-        aRelation = CreateDummyRelationPeer()        
+        aSurvey = Survey.create('dummyEmployName', 'dummyEmployLastName', 'dummyEmploy@myEmail.com')
+        aRelation = RelationType.create('DummyPeer', 'A dummy colleague', aSurvey)
+          
         self.assertEqual('DummyPeer', aRelation.__str__())
         
-    def test_FeedbackProvider(self):
-        aFeedbackProvider = CreateDummmyFeedbackProvider()
-        aFeedbackProvider.last_name = 'DummylastName'
+    def test_FeedbackProvider__str__(self):
+        aSurvey = Survey.create('dummyEmployName', 'dummyEmployLastName', 'dummyEmploy@myEmail.com')
+        aRelation = RelationType.create('DummyPeer', 'A dummy colleague', aSurvey)
+        aFeedbackProvider = FeedbackProvider.create('DummyName', 'DummylastName', 'dummy@myEmail.com', aRelation, aSurvey)
+
         self.assertEqual('DummyName DummylastName (dummy@myEmail.com)', aFeedbackProvider.__str__())
         
     def test_FeedbackProvider_can_have_empty_LastName(self):
-        aFeedbackProvider = CreateDummmyFeedbackProvider()
-        self.assertEqual('', aFeedbackProvider.last_name)
+        aSurvey = Survey.create('dummyEmployName', 'dummyEmployLastName', 'dummyEmploy@myEmail.com')
+        aRelation = RelationType.create('DummyPeer', 'A dummy colleague', aSurvey)
+        aFeedbackProvider = FeedbackProvider.create('DummyName', '', 'dummy@myEmail.com', aRelation, aSurvey)
+        
         self.assertEqual('DummyName  (dummy@myEmail.com)', aFeedbackProvider.__str__())
+        
+    def test_Can_not_create_two_FeedbackProviders_with_same_email_for_asurvey(self):
+        aSurvey = Survey.create('dummyEmployName', 'dummyEmployLastName', 'dummyEmploy@myEmail.com')
+        aRelation = RelationType.create('DummyPeer', 'A dummy colleague', aSurvey)
+        a1stFeedbackProvider = FeedbackProvider.create('A_name', 'A_lastname', 'dummy@myEmail.com', aRelation, aSurvey)
+        
+        try:     
+            FeedbackProvider.create('An_other_name', 'An_other_lastname', 'dummy@myEmail.com', aRelation, aSurvey)
+            self.fail("An IntegrityError was expected.")    
+        except IntegrityError:
+            self.assertEqual('A_name A_lastname (dummy@myEmail.com)', a1stFeedbackProvider.__str__())
+            pass        
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
